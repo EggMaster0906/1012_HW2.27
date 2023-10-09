f8e1159866f9c5f3c562908de8e89527805bcc6a
EggMaster0906 <lsinpen@gmail.com>
2023-10-09 11:36:38 +0800

    first

---
+++ source/main.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int high = 5;
+	
+	for (int i = 1; i <= high; i++)
+	{
+		for (int blank = 1; blank <= high-i; blank++)
+		{
+			printf(" ");
+		}
+		for (int star = 1; star <= 2*i-1; star++)
+		{
+			printf("*");
+		}
+
+		printf("\n");
+	}
+}
